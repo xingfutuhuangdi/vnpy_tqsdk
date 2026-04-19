0f68dc4e4d6bb4638963f5036cb56c5161701a27
qiuchun <362460803@qq.com>
2026-04-20 00:19:02 +0800

    [Mod]更换获取数据的api

--- vnpy_tqsdk/tqsdk_datafeed.py
+++ vnpy_tqsdk/tqsdk_datafeed.py
@@ -2,8 +2,9 @@ from datetime import timedelta, datetime
 from collections.abc import Callable
 import traceback
 
-from pandas import DataFrame
-from tqsdk import TqApi, TqAuth
+# from pandas import DataFrame
+import pandas as pd
+from tqsdk import TqApi, TqAuth, TqBacktest, BacktestFinished
 
 from vnpy.trader.datafeed import BaseDatafeed
 from vnpy.trader.setting import SETTINGS
@@ -43,37 +44,38 @@ class TqsdkDatafeed(BaseDatafeed):
         if not interval:
             output(f"Tqsdk查询K线数据失败：不支持的时间周期{req.interval.value}")
             return []
-
-        tq_symbol: str = f"{req.exchange.value}.{req.symbol}"
-
-        df: DataFrame = api.get_kline_data_series(
-            symbol=tq_symbol,
-            duration_seconds=interval,
-            start_dt=req.start,
-            end_dt=(req.end + timedelta(1))
-        )
-
-        # 关闭API
-        api.close()
-
+        
         # 解析数据
         bars: list[BarData] = []
 
-        if df is not None:
-            for tp in df.itertuples():
-                bar: BarData = BarData(
-                    symbol=req.symbol,
-                    exchange=req.exchange,
-                    interval=req.interval,
-                    datetime=datetime.fromtimestamp(tp.datetime/1_000_000_000, tz=CHINA_TZ),    # type: ignore
-                    open_price=tp.open,
-                    high_price=tp.high,
-                    low_price=tp.low,
-                    close_price=tp.close,
-                    volume=tp.volume,
-                    open_interest=tp.open_oi,
-                    gateway_name="TQ",
-                )
-                bars.append(bar)
+        tq_symbol: str = f"{req.exchange.value}.{req.symbol}"
+
+        df: pd.DataFrame = api.get_kline_serial(
+            symbol=tq_symbol,
+            duration_seconds=INTERVAL_VT2TQ[req.interval]
+        )
+
+        try:
+            while api.wait_update():
+                if api.is_changing(df.iloc[-1], "datetime"):
+                    tp = df.iloc[-1]
+                    bar: BarData = BarData(
+                        symbol=req.symbol,
+                        exchange=req.exchange,
+                        interval=req.interval,
+                        datetime=datetime.fromtimestamp(tp.datetime/1_000_000_000, tz=CHINA_TZ),    # type: ignore
+                        open_price=tp.open if not pd.isna(tp.open) else 0.0,
+                        high_price=tp.high if not pd.isna(tp.high) else 0.0,
+                        low_price=tp.low if not pd.isna(tp.low) else 0.0,
+                        close_price=tp.close if not pd.isna(tp.close) else 0.0,
+                        volume=tp.volume,
+                        open_interest=tp.open_oi,
+                        gateway_name="TQ",
+                    )
+                    bars.append(bar)
+                    
+        except BacktestFinished as e:
+            output(f"回测结束")
+            api.close()
 
         return bars
